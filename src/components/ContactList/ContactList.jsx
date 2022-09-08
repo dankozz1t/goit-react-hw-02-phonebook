f8cd5eba3a30616fe4303d82cc89a0510ec1c7cb
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-08 11:45:50 +0300

    add ContactItem

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,4 @@
+import ContactItem from 'components/ContactItem/ContactItem';
 import React, { Component } from 'react';
 
 export default class ContactList extends Component {
@@ -6,13 +7,14 @@ export default class ContactList extends Component {
       <div>
         {this.props.contacts.length > 0 && (
           <ul>
-            {this.props.contacts.map(({ id, name, number }) => {
-              return (
-                <li key={id}>
-                  <span>{name}</span>: <span>{number}</span>
-                </li>
-              );
-            })}
+            {this.props.contacts.map(({ id, name, number }) => (
+              <ContactItem
+                key={id}
+                name={name}
+                number={number}
+                handleClickDelete={this.props.handleClickDelete}
+              />
+            ))}
           </ul>
         )}
       </div>
